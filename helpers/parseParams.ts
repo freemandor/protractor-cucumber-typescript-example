2c8e754f340e5b79d80c98d5d60c097980d0f2de
Dor Yaron <dor.yaron@here.com>
2020-08-19 20:42:58 +0300

    added savedData.ts to enable sharing responses data between steps

--- helpers/parseParams.ts
+++ helpers/parseParams.ts
@@ -1,3 +1,27 @@
-export class ParsedParams {
-    public 
+import { World } from '../step_definitions/world/world'
+import { binding } from 'cucumber-tsflow/dist'
+
+@binding([World])
+export class ParsedParamsFunctions {
+    protected world: World
+    constructor(world: World)
+    {this.world = world }
+
+    public __getCreateEmployeeId(): string {
+        const response = this.world.savedData.getCreateEmployeeResponse()
+
+        return response.data.data.id
+    }
+
+    public parse(params: {[key: string]: string}): {[key: string]: string} {
+        const parsedParams = {}
+        for (const param in params) {
+            if (params[param].startsWith('__')) {
+                parsedParams[param] = this[params[param]]()
+            } else {
+                parsedParams[param] = params[param]
+            }
+        }
+        return parsedParams
+    }
 }
--- step_definitions/world/savedData.ts
+++ step_definitions/world/savedData.ts
@@ -2,6 +2,7 @@ import { AxiosResponse } from "axios";
 
 export class SavedData {
     private lastResponse?: AxiosResponse
+    private createEmployeeResponse?: AxiosResponse
 
     public setLastResponse(response: AxiosResponse) {
         this.lastResponse = response
@@ -9,5 +10,13 @@ export class SavedData {
 
     public getLastResponse(): AxiosResponse {
         return this.lastResponse
+    }
+
+    public getCreateEmployeeResponse(): AxiosResponse {
+        return this.createEmployeeResponse
+    }
+
+    public setCreateEmployeeResponse(response: AxiosResponse) {
+        this.createEmployeeResponse = response
     }
 }
